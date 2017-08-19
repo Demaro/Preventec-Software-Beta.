7b3e95cadaf3e94128d899375f9a1904d5cc7a7f
Demaro <demaromail@gmail.com>
2017-08-18 23:26:11 -0300

    new templates 3

--- accounts/views.py
+++ accounts/views.py
@@ -8,31 +8,37 @@ from django.contrib.auth import (
 
 User = get_user_model()
 
+from django.utils import timezone
+
 from django.shortcuts import render, redirect
 
 from django.http import HttpResponseRedirect, HttpResponse
 
 from .forms import UserLoginForm, UserRegisterForm
 
+from profiles.forms import ProfileForm
+
+from profiles.models import Profile
+
 def login_view(request):
-    print(request.user.is_authenticated())
-    next = request.GET.get('next')
-    title = "Login"
+
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/principal')
     form = UserLoginForm(request.POST or None)
+
     if form.is_valid():
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get('password')
         user = authenticate(username=username, password=password)
         login(request, user)
-        if next:
-            return redirect(next)
         return redirect("/principal")
-    return render(request, "login.html", {"form":form, "title": title})
+    return render(request, "login.html", {"form":form})
 
 
 def register_view(request):
         if request.user.is_authenticated():
             form = UserRegisterForm(request.POST or None)
+            form2 = ProfileForm(request.POST or None)
         if form.is_valid():
 
             username = form.cleaned_data.get("username")
@@ -45,10 +51,28 @@ def register_view(request):
             #login(request, new_user)
             new_user.id
             print(new_user.id)
-            return HttpResponseRedirect('crear_perfil/%s/' % new_user.id)
 
+        if form2.is_valid():
 
-        return render(request, "user.html", )
+            rut_data = form2.cleaned_data.get("rut")
+            birthdate_data = form2.cleaned_data.get("birthdate")
+            avatar_data = form2.cleaned_data.get("avatar")
+            cargo_data = form2.cleaned_data.get("cargo")
+            especialidad_data = form2.cleaned_data.get("especialidad")
+            contrato_data = form2.cleaned_data.get("contrato")
+            legales_asoc_data = form2.cleaned_data.get("legales_asoc")
+
+            new_profile = Profile(user_id=new_user.id, rut=rut_data, birthdate=birthdate_data, avatar=avatar_data, cargo=cargo_data, especialidad=especialidad_data, contrato=contrato_data, legales_asoc=legales_asoc_data, ultimateupdate = timezone.now(), inicio_cargo=timezone.now())
+
+            new_profile.save()
+            new_profile.id
+            print(new_profile.id)
+            return HttpResponseRedirect('/principal')
+
+        context = {
+        "form2": form2,
+        }
+        return render(request, "user.html", context)
 
 
 def logout_view(request):
