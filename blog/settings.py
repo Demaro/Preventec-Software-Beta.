945bd39c3693d70aa243e9bdb9e25088e2bfa616
David Martinez Rojas <demaro@MacBook-Air-de-David.local>
2017-10-20 21:59:22 -0300

    deploy 20/10

--- blog/settings.py
+++ blog/settings.py
@@ -161,16 +161,16 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATIC_URL = '/static/'
 
-"""
+
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, "static"),
 	#'/var/www/static/',
 ]
 
-"""
 
 
-#STATICFILES_STORAGE ='django.contrib.staticfiles.storage.CachedStaticFilesStorage'
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
